680e78f1ef5f32e319cfd94592b5f9e57e354922
JamesSullivan1 <calgaryjames@gmail.com>
2014-09-30 17:25:43 -0600

    cleaned up

--- Makefile
+++ Makefile
@@ -3,9 +3,12 @@ CFLAGS=-g -Wall
 
 all: clean inject
 
-inject: inject.c
-	$(CC) $(CFLAGS) inject.c -o inject 
+inject: inject.c elf_tools.o elf_tools.h
+	$(CC) $(CFLAGS) inject.c elf_tools.o -o inject 
+
+elf_tools.o: elf_tools.c elf_tools.h
+	$(CC) $(CFLAGS) -c elf_tools.c elf_tools.h
 
 clean:
-	rm -rf *.o inject
+	rm -rf *.o *.gch inject
 
---
+++ elf_tools.h
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+
+/* Contains useful macros and definitions for elf_tools */
+#if __x86_64__
+#define UWORD unsigned long long
+#define WORD long long
+#define ARCH_JUMP_INSTR '\xe9'
+#define ARCH_REDIR_SZ 5
+#define ARCH_BASE_ADDR 0x400000
+#define ElfN_Ehdr    Elf64_Ehdr
+#define ElfN_Shdr    Elf64_Shdr
+#define ElfN_Phdr    Elf64_Phdr
+#define ElfN_Dyn     Elf64_Dyn
+#define ElfN_Rel     Elf64_Rel
+#define ElfN_Rela    Elf64_Rela
+#define ElfN_Sym     Elf64_Sym
+#define print_addr(format, ...) do {            \
+    printf(format "0x%016Lx",                   \
+            (unsigned long long)__VA_ARGS__);   \
+} while(0)
+#else
+#define UWORD unsigned long
+#define WORD long 
+#define ARCH_JUMP_INSTR '\xe9'
+#define ARCH_REDIR_SZ 5
+#define ARCH_BASE_ADDR 0x8000000
+#define ElfN_Ehdr    Elf32_Ehdr
+#define ElfN_Shdr    Elf32_Shdr
+#define ElfN_Phdr    Elf32_Phdr
+#define ElfN_Dyn     Elf32_Dyn
+#define ElfN_Rel     Elf32_Rel
+#define ElfN_Rela    Elf32_Rela
+#define ElfN_Sym     Elf64_Sym
+#define print_addr(format, ...) do {            \
+    printf(format "0x%08x",                     \
+            (unsigned int)__VA_ARGS__);         \
+} while(0)
+#endif
+
+int inject_section(
+        char *elf, size_t elf_sz,
+        char *dat, size_t dat_sz,
+        const char *sect_name,
+        char **dst);
